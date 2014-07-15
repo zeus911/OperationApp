5276866ee184f4dda2cd844356e56a6afab300a2
ea7son <ea7son@gmail.com>
2014-07-15 10:33:41 +0800

    111

---
+++ Nbugs.IDAL/Db144/IRoleModuleActionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Nbugs.IDAL;
+using Nbugs.OperationApp.Models.System;
+
+namespace Nbugs.IDAL.Db144
+{
+    public interface IRoleModuleActionRepository : IBaseRepository<RoleModuleAction>
+    {
+    }
+}
